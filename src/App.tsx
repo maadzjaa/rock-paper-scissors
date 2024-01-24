bb2bbaf9fe933b77961305f289411372f77198cd
maadzjaa <magda.g2112@gmail.com>
2024-01-24 17:22:54 +0100

    feat: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,58 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 enum Choice {
-	Rock = 'rock',
-	Paper = 'paper',
-	Scissors = 'scissors',
+  Rock = "rock",
+  Paper = "paper",
+  Scissors = "scissors",
 }
 
 function App() {
-	const [player1, setPlayer1] = useState<string | null>(null);
-	const [player2, setPlayer2] = useState<string | null>(null);
-	const [result, setResult] = useState<string | null>(null);
+  const [player1, setPlayer1] = useState<string | null>(null);
+  const [player2, setPlayer2] = useState<string | null>(null);
+  const [result, setResult] = useState<string | null>(null);
 
-	function handlePlayerChoice(value: string): void {
-		if (!player1) {
-			setResult(null);
-			setPlayer1(value);
-		} else {
-			setPlayer2(value);
-			setResult(checkResult(player1, value));
-			setPlayer1(null);
-			setPlayer2(null);
-		}
-	}
+  function handlePlayerChoice(value: string): void {
+    if (!player1) {
+      setResult(null);
+      setPlayer1(value);
+    } else {
+      setPlayer2(value);
+      setResult(checkResult(player1, value));
+      setPlayer1(null);
+      setPlayer2(null);
+    }
+  }
 
-	function checkResult(player1: string | null, player2: string | null) {
-		if (player1 === player2) {
-			return 'Draw!';
-		}
+  function checkResult(player1: string | null, player2: string | null) {
+    if (player1 === player2) {
+      return "Draw!";
+    }
 
-		if (player1 === Choice.Paper && player2 === Choice.Rock) {
-			return 'Player 1 won!';
-		}
-		if (player1 === Choice.Scissors && player2 === Choice.Paper) {
-			return 'Player 1 won!';
-		}
-		if (player1 === Choice.Rock && player2 === Choice.Scissors) {
-			return 'Player 1 won!';
-		}
+    if (
+      (player1 === Choice.Paper && player2 === Choice.Rock) ||
+      (player1 === Choice.Scissors && player2 === Choice.Paper) ||
+      (player1 === Choice.Rock && player2 === Choice.Scissors)
+    ) {
+      return "Player 1 won!";
+    }
 
-		return 'Player 2 won!';
-	}
+    return "Player 2 won!";
+  }
 
-	return (
-		<>
-			<p>{result ? 'Play again :)' : ':)'}</p>
-			<p>
-				{!player1 && 'First player!'}
-				{!player2 && player1 && 'Second player!'}
-			</p>
-			<button onClick={() => handlePlayerChoice(Choice.Paper)}>Paper</button>
-			<button onClick={() => handlePlayerChoice(Choice.Scissors)}>Scissors</button>
-			<button onClick={() => handlePlayerChoice(Choice.Rock)}>Rock</button>
-			<p>{result}</p>
-		</>
-	);
+  return (
+    <>
+      <p>{result ? "Play again :)" : ":)"}</p>
+      <p>
+        {!player1 && "First player!"}
+        {!player2 && player1 && "Second player!"}
+      </p>
+      <button onClick={() => handlePlayerChoice(Choice.Paper)}>Paper</button>
+      <button onClick={() => handlePlayerChoice(Choice.Scissors)}>Scissors</button>
+      <button onClick={() => handlePlayerChoice(Choice.Rock)}>Rock</button>
+      <p>{result}</p>
+    </>
+  );
 }
 
 export default App;
